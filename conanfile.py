8849dd61c04c56968199c151bd3fa33cea3e84b0
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2017-12-28 14:24:19 +0100

    disable linux build as it contains hardcoded reference to swiglib (does not work with conan) kept windows variant as it does search a path relative to the swig.exe binary (which should work with conan.)

--- conanfile.py
+++ conanfile.py
@@ -8,18 +8,7 @@ class SwigConan(ConanFile):
     url = "https://github.com/ulricheck/conan-swig"
     description = "SWIG is a software development tool that connects programs written in C and C++ with a variety of high-level programming languages. http://www.swig.org"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
     generators = "cmake"
-
-    def source(self):
-        if self.settings.os == "Windows":
-            # swig will only be built if not on windows
-            pass
-        else:
-            source_file = "swig-rel-%s" % self.version
-            tools.get("https://github.com/swig/swig/archive/rel-%s.zip" % self.version)
-            os.rename(source_file, "source")
 
 
     def build(self):
@@ -28,24 +17,19 @@ class SwigConan(ConanFile):
             tools.get("https://downloads.sourceforge.net/project/swig/swigwin/%s/%s.zip" % (source_file, source_file))
             os.rename(source_file, "source")
         else:
-            from conans import AutoToolsBuildEnvironment
-            env_build = AutoToolsBuildEnvironment(self)
-            
-            with tools.environment_append(env_build.vars):
-                self.run("cd source && sh autogen.sh")
-                self.run("cd source && ./configure --prefix=%s/install" % self.install_folder)
-                self.run("cd source && make && make install")
+            raise ValueError("Swig Build dependency only support on Windows = please install swig on other systems using your package manager")
 
 
     def package(self):
-        self.copy("*", dst="bin", src="install/bin")
-        self.copy("*", dst="share", src="install/share")
+        if self.settings.os == "Windows":
+            self.copy("swig.exe", dst=".", src="source")
+            self.copy("*", dst="Lib", src="source/Lib")
+        else:
+            raise ValueError("Swig Build dependency only support on Windows = please install swig on other systems using your package manager")
 
     def package_info(self):
         swig_home = os.path.join(self.package_folder)
         self.output.info("Creating SWIG_HOME environment variable with : {0}".format(swig_home))
         self.env_info.SWIG_HOME = swig_home
-
-        bin_path = os.path.join(swig_home, "bin")
-        self.output.info("Appending PATH environment variable with : {0}".format(bin_path))
-        self.env_info.path.append(bin_path)
+        self.output.info("Appending PATH environment variable with : {0}".format(swig_home))
+        self.env_info.path.append(swig_home)
